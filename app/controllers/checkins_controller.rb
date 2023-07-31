747a4ae6018a39da658222e4fcde1c001d693088
Test <test@Tests-MacBook-Pro.local>
2023-07-31 15:34:48 +0300

    add attendance ui

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,6 +1,7 @@
 class CheckinsController < ApplicationController
   
   def index
+    @admin = current_admin
     @checkins = Checkin.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     @member = Member.all
   end
