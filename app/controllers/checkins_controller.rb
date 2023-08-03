255ae6da3774f063fb20666c44ccc15a16a9e262
Test <test@Tests-MacBook-Pro.local>
2023-08-03 11:37:17 +0300

    add restriction to check in

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -13,10 +13,13 @@ class CheckinsController < ApplicationController
 
   def create
     @member = Member.find(params[:id])
-    if @member.checkins.create(paid: false)
+    if !Checkin.exists?(member_id: 10,created_at: Time.now.beginning_of_day..Time.now.end_of_day) == true
+      @member.checkins.create(paid: false)
       redirect_to checkins_path
     else
-      render 'new' , status: :unprocessable_entity
+      puts "Tumefikiwa nani"
+       flash[:notice] = "User has already Checked In"
+      redirect_to new_checkin_path
     end
   end
 end
