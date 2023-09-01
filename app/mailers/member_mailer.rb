468921a87f7963f0e3f2bcc50dad5a4875e12f88
Test <test@Tests-MacBook-Pro.local>
2023-09-01 18:23:59 +0300

    Adds number of checkin email

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -8,4 +8,12 @@ class MemberMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Your fitness Journey just got better')
   end
+
+  def checkin_metrics(member, no_of_checkins, gym_name)
+    @user = member
+    @checkins = no_of_checkins
+    @gym_name = gym_name
+
+    mail(to: @user.email, subject: 'Congratulations')
+  end
 end
